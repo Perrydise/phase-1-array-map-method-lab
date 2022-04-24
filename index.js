6f2ce49c0d02c5cef9ded373ef0f0ca550bdda3a
Perry Jorss <pjorss123@gmail.com>
2022-04-24 18:24:08 -0400

    commit tree 0aab67cbc119bfd8431bc490b95f98dee1f76a41

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map((title) => {
+    title.split(" ");
+    const sentSplit = title.split(" ");
+    console.log(sentSplit);
+   let split = sentSplit.map((word) => {
+      console.log(word.charAt(0).toUpperCase())
+      let upSent = word.charAt(0).toUpperCase()
+      let remainder = word.substring(1)
+      return upSent + remainder
+    });
+    return split.join(' ')
+  });
 }
